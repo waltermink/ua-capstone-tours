cc1f47a54fa823e8d99b8eebee036025c967af05
Walter Mink <wwmink@crimson.ua.edu>
2026-02-17 14:41:46 -0600

    Changed media storage to be bound, now photos and files are not tied to the docker container, we might need to change this later though

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -23,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "your-secret-key-for-dev-only")
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
