745a49a18211985d5161273bce9e3956e9c7eeee
jacob_crimson <jchilders3@crimson.ua.edu>
2026-02-18 10:18:50 -0600

    moved map files into backend folder to so that it can be run with docker

---
+++ backend/static/map/map.js
@@ -0,0 +1,45 @@
+let map;
+let buildings = [];
+let audio = new Audio();
+let userMarker;
+
+function initMap() { //leaflet's website is used for this section
+    map = L.map("Map").setview([33.214, -87.545], 16)
+
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+        maxZoom: 19,
+    }).addTo(map);
+}
+
+function loadBuildings() {
+    fetch("/api/buildings/")
+    .then(res => res.json())
+    .then(data=> {
+        buildings = data.map(b => ({
+            ...b, 
+            triggered: false,
+            marker: L.marker([b.latitude, b.longitude])
+        }));
+    });
+}
+
+function startTour() { // Begins live tracking
+    navigator.geolocation.watchPosition(updatePosition, error, {
+        enableHighAccuracy: true,
+        maximumAge: 5000,
+        timeout: 10000
+    });
+}
+
+function updatePosition(position) {
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+
+    if (!userMarker) {
+        userMarker = L.marker([lat, lon]).addTo(map);
+    }
+    else{
+        userMarker.setLatLng([lat, lon]);
+    }
+    checkGeofences(lat, lon);
+}
