eca06f65c4614f5f5828c883c534e165d55ca549
Walter Mink <wwmink@crimson.ua.edu>
2026-03-22 13:40:55 -0500

    Minor fixes to remove storybook

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,6 +1,4 @@
 // For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
 import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
@@ -26,4 +24,4 @@ export default defineConfig([globalIgnores(['dist']), {
   rules: {
     'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
   },
-}, ...storybook.configs["flat/recommended"]])
+}])
