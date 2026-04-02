69fba128491bb77c65390b93ccdca820d46f96ab
Walter Mink <wwmink@crimson.ua.edu>
2026-04-02 17:17:53 -0500

    Added more to Contribute page, also updated map screen to hopefully span all avaliable space

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,29 @@ import ToursPage from './pages/ToursPage.jsx';
 import ContributePage from './pages/ContributePage.jsx';
 
 function App() {
-    const [activeTab, setActiveTab] = useState('explore');
+    const [activeTab, setActiveTab] = useState('contribute');
+    const [contributeStep, setContributeStep] = useState(0);
+    const [contributeData, setContributeData] = useState({
+        loc_name: '',
+        loc_type: '',
+        loc_addr: '',
+        loc_short_desc: '',
+        loc_long_desc: '',
+        loc_lat: '',
+        loc_lon: '',
+        loc_media: ''
+    });
 
     if (activeTab === 'list')        return <ListPage activeTab={activeTab} onTabChange={setActiveTab} />;
     if (activeTab === 'tours')       return <ToursPage activeTab={activeTab} onTabChange={setActiveTab} />;
-    if (activeTab === 'contribute')  return <ContributePage activeTab={activeTab} onTabChange={setActiveTab} />;
+    if (activeTab === 'contribute')  return <ContributePage 
+                                                activeTab={activeTab} 
+                                                onTabChange={setActiveTab} 
+                                                contributeStep={contributeStep}
+                                                setContributeStep={setContributeStep}
+                                                contributeData={contributeData}
+                                                setContributeData={setContributeData}
+                                            />;
 
     return <ExplorePage activeTab={activeTab} onTabChange={setActiveTab} />;
 }
--- frontend/src/pages/ContributePage.jsx
+++ frontend/src/pages/ContributePage.jsx
@@ -1,10 +1,15 @@
 import Navbar from '../components/navbar.jsx';
 import ContributeView from '../components/contributeView.jsx';
 
-function ContributePage({ activeTab, onTabChange }) {
+function ContributePage({activeTab, onTabChange, contributeStep, setContributeStep, contributeData, setContributeData}) {
     return (
         <div style={{ height: '100svh', display: 'flex', flexDirection: 'column' }}>
-            <ContributeView/>
+            <ContributeView 
+                step={contributeStep} 
+                onStepChange={setContributeStep}
+                contributeData={contributeData}
+                setContributeData={setContributeData}
+            />
             <Navbar activeTab={activeTab} onTabChange={onTabChange} />
         </div>
     );
