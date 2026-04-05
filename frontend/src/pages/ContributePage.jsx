18533ae2182e31adadadfcfd960633ba21a2a5f0
Walter Mink <wwmink@crimson.ua.edu>
2026-04-05 18:08:30 -0500

    Implemented basically all of the UI for user submissions, made a list of landmarks to add, added inaccessible docs pages to index

--- frontend/src/pages/ContributePage.jsx
+++ frontend/src/pages/ContributePage.jsx
@@ -3,14 +3,16 @@ import ContributeView from '../components/contributeView.jsx';
 
 function ContributePage({activeTab, onTabChange, contributeStep, setContributeStep, contributeData, setContributeData}) {
     return (
-        <div style={{ height: '100svh', display: 'flex', flexDirection: 'column' }}>
+        <div style={{ height: '100svh', display: 'flex', flexDirection: 'column'}}>
             <ContributeView 
                 step={contributeStep} 
                 onStepChange={setContributeStep}
                 contributeData={contributeData}
                 setContributeData={setContributeData}
             />
-            <Navbar activeTab={activeTab} onTabChange={onTabChange} />
+            <div style={{ position: 'fixed', bottom: 0, left: 0, right: 0, zIndex: 820 }}>
+                <Navbar activeTab={activeTab} onTabChange={onTabChange} />
+            </div>
         </div>
     );
 }
