31d86f43bdc1d1d848c05811d1701a6921a2e9a6
jacob_crimson <jchilders3@crimson.ua.edu>
2026-04-16 07:19:42 -0500

    security warning fixed

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -82,6 +82,14 @@ REST_FRAMEWORK = {
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.AllowAny",
     ],
+    "DEFAULT_THROTTLE_CLASSES": [
+        "rest_framework.throttling.AnonRateThrottle",
+        "rest_framework.throttling.UserRateThrottle",
+    ],
+    "DEFAULT_THROTTLE_RATES": {
+        "anon": "100/hour",
+        "user": "1000/hour",
+    },
 }
 
 TEMPLATES = [
